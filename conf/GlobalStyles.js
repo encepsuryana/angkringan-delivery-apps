113d2f21a268f4e565ce2c77734286b3c9c54597
encepsuryana <encep.suryanajr@gmail.com>
2022-01-28 16:56:33 +0700

    Add Package and SearchBar.js

--- conf/GlobalStyles.js
+++ conf/GlobalStyles.js
@@ -2,6 +2,7 @@ import { StyleSheet, Platform } from "react-native";
 export default StyleSheet.create({
   droidSafeArea: {
     flex: 1,
+    backgroundColor: "#eee",
     paddingTop: Platform.OS === "android" ? 35 : 0,
   },
 });
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,15 @@
 import { View, Text, SafeAreaView } from "react-native";
 import GlobalStyles from "../conf/GlobalStyles";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
     <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <HeaderTabs />
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
     </SafeAreaView>
   );
 }
